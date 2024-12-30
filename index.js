db9defa029868e0166e1c2da1dade472eafbed87
Onkar Kadam <onkar.kadam@lightspeedhq.com>
2024-12-30 13:46:36 -0500

    Update init 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hello this is a test {name}!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
